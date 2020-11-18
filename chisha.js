5cc78ef411b6169de676063c10f043c513b0effe
chisha <aamaechi@hotmail.com>
2020-11-18 22:13:11 +0000

    Fixed tabs and tab content taht was not showing before

--- chisha.js
+++ chisha.js
@@ -1,20 +1,38 @@
-const tabs = document.querySelectorAll('[data-tab-target]'); 
-//create variable called tabs - grab all tabs
-const tabContents = document.querySelectorAll('[data-tab-content]');
-//create variable called tabContents - grab all tabContents words
+// let tabs = document.querySelectorAll('[data-tab-target]')
+// //create variable called tabs - grab all tabs
+// let tabContents = document.querySelectorAll('[data-tab-content]')
+// //create variable called tabContents - grab all tabContents words
 
-tabs.forEach(tab => {                  //4Loop Each time you click on tab fire function
-    tab.addEventListener('click', () => { //Function = when click on tab do something...
-        const target = document.querySelector(tab.dataset.tabTarget) //new variable called target - grab all tabs
-        tabContents.forEach(tabContent => {                         //4Loop Each tabContents remove others that are not active
-            tabContent.classList.remove('active')
-        })
+// tabs.forEach(tab => {                  //4Loop Each time you click on tab fire function
+//     tab.addEventListener('click', () => { //Function = when click on tab do something...
+//         let target = document.querySelector(tab.dataset.tabTarget) //new variable called target - grab all tabs
+//         tabContents.forEach(tabContent => {                         //4Loop Each tabContents remove others that are not active
+//             tabContent.classList.remove('active')
+//         })
 
-        tabs.forEach(tab => {              //4Loop Each time tab button clicked on
-            tab.classList.remove('active') //Removes previous active
-        })
-        tab.classList.add('active')        //Clicked on becomes active
+//         tabs.forEach(tab => {              //4Loop Each time tab button clicked on
+//             tab.classList.remove('active') //Removes previous active
+//         })
+//         tab.classList.add('active')        //Clicked on becomes active
 
-    target.classList.add('active')        //Add clicked on active tab to show
+//     target.classList.add('active')        //Add clicked on active tab to show
+//     })
+// })
+
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
     })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
 })
